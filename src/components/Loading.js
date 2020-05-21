404e1fc3ca70c15bd8add8e3d3c3221e528a8276
Kurt galvin <kurtgalvin@Kurts-MacBook-Pro.local>
2020-05-21 14:46:23 -0700

    Create Dashboard

---
+++ src/components/Loading.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Loading extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <section className="loading">Loading</section>
+    )
+  }
+}
